89d91b525f45327668b09d7d4d35efa9f18ebec2
Xin Long <lucien.xin@gmail.com>
2017-06-17 02:10:15 +0800

    testcase: add repeatability on ipsec
    This patch is to ake repeatability sctp test available on ipsec
    
    Note that it is to set with "proto ah" for ipsec, which is different
    from the def opt. We need to define Topo_opt in this testcase.
    
    Signed-off-by: Xin Long <lucien.xin@gmail.com>
    Signed-off-by: Marcelo Ricardo Leitner <marcelo.leitner@gmail.com>

---
+++ testcase/performance/repeatability/sctp_stream~on_ipsec.sh
@@ -0,0 +1,5 @@
+source "./sctp_stream.sh"
+Name="sctp repeatability - sctp-steam on ipsec" # Case Name, demand
+Topo_opt="proto ah spi 0x1000 mode transport enc blowfish ipv6readylogo3descbc1to2 auth hmac\(sha1\) ipv6readylogsha11to2 | \
+	  proto ah spi 0x2000 mode transport enc blowfish ipv6readylogo3descbc2to1 auth hmac\(sha1\) ipv6readylogsha12to1"
+Topo="netns_ipsec_cs" # Optional
